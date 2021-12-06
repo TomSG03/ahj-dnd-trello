42d870313867a78e14edad8efaa25e40e53f18dc
Tomilov Sergey <tomexpert@yandex.ru>
2021-12-06 23:02:15 +0800

    v0.4

--- src/js/trello.js
+++ src/js/trello.js
@@ -3,7 +3,7 @@ export default class Trello {
     this.dom = dom;
     this.topic = topic;
     this.dragItem = null;
-    this.target = null;
+    this.source = null;
   }
 
   init(arrExample) {
@@ -18,7 +18,7 @@ export default class Trello {
     for (let i = 0; i < this.topic.length; i += 1) {
       const list = document.createElement('div');
       list.className = 'list-block';
-      list.innerHTML = `<h4>${this.topic[i]}</h3>`;
+      list.innerHTML = `<h4 class='topic'>${this.topic[i]}</h3>`;
       list.append(this.colFill(this.topic[i], i));
       list.append(this.addRec());
       this.dom.append(list);
@@ -52,87 +52,9 @@ export default class Trello {
     this.colsFill();
     this.createCap();
 
-    this.dom.addEventListener('mousedown', (e) => {
-      if (e.target.classList.contains('btn-del')) { return; }
-      const target = e.target.closest('.list-item');
-      if (!target) {
-        return;
-      }
-
-      if (e.target.classList.contains('list-item')) {
-        this.target = target;
-        e.target.style.cursor = 'grabbing';
-        // const source = document.elementFromPoint(e.clientX, e.clientY);
-        // console.log(`H: ${source.offsetHeight} | W: ${source.offsetWidth} | Y: ${e.pageY - source.offsetTop} | X: ${e.pageX - source.offsetLeft}`);
-
-        this.dragItem = this.target.cloneNode(true);
-        this.dom.appendChild(this.dragItem);
-        this.dragItem.style.top = `${e.pageY - this.target.offsetHeight / 2}px`;
-        this.dragItem.style.left = `${e.pageX - this.target.offsetWidth / 2}px`;
-        this.dragItem.style.width = `${this.target.offsetWidth}px`;
-        this.dragItem.style.zIndex = '5000';
-        this.dragItem.style.position = 'absolute';
-        this.dragItem.style.opacity = '0.8';
-        this.dragItem.style.pointerEvents = 'none';
-
-        this.target.classList.add('selected');
-      }
-    });
-
-    this.dom.addEventListener('mouseup', (e) => {
-      if (!this.target) {
-        return;
-      }
-      this.dom.style.cursor = 'default';
-      this.dom.querySelector('.list-block').style.cursor = 'default';
-      const insertItem = document.elementFromPoint(e.clientX, e.clientY);
-      if (insertItem.classList.contains('list-item-cap')) {
-        this.target.visibility = 'visible';
-        this.cap.replaceWith(this.target);
-      }
-
-      if (this.cap) {
-        this.cap.remove();
-      }
-
-      this.dragItem.remove();
-      this.target.classList.remove('selected');
-      this.dragItem = null;
-      this.target = null;
-    });
-
-    this.dom.addEventListener('mouseout', (e) => {
-      if (e.target.classList.contains('list-item')) {
-        e.target.style.cursor = 'grab';
-      }
-    });
-
-    this.dom.addEventListener('mousemove', (e) => {
-      if (!this.target) {
-        return;
-      }
-      e.target.style.cursor = 'grabbing';
-      this.dragItem.style.left = `${e.pageX - this.dragItem.offsetWidth / 2}px`;
-      this.dragItem.style.top = `${e.pageY - this.dragItem.offsetHeight / 2}px`;
-      const insertItem = document.elementFromPoint(e.clientX, e.clientY);
-      if (insertItem.classList.contains('list-item') && insertItem !== this.target && insertItem.previousSibling !== this.target && insertItem.nextSibling !== this.target) {
-        const midlle = insertItem.offsetHeight / 2;
-        const currentPos = e.pageY - insertItem.offsetTop;
-        this.cap.style.height = `${this.dragItem.offsetHeight}px`;
-        if (currentPos > midlle) {
-          insertItem.insertAdjacentElement('afterEnd', this.cap);
-        }
-        if (currentPos < midlle) {
-          insertItem.insertAdjacentElement('beforeBegin', this.cap);
-        }
-      }
-      if (insertItem.classList.contains('addRec')) {
-        insertItem.closest('.list-block').querySelector('ul').append(this.cap);
-      }
-    });
-
+    // ----------------------- click -----------------------------
     this.dom.addEventListener('click', (e) => {
-      // e.target.style.cursor = 'default';
+      e.target.style.cursor = 'default';
       if (e.target.classList.contains('addRec')) {
         e.target.closest('.list-block').append(this.formAddRec());
         e.target.remove();
@@ -155,10 +77,95 @@ export default class Trello {
           e.target.closest('.list-block').querySelector('ul').append(li);
           e.target.closest('.list-block').append(this.addRec());
           e.target.closest('.formAddRec').remove();
+          this.saveState();
+        } else {
+          const err = e.target.form.querySelector('textarea');
+          err.setAttribute('placeholder', 'Нужно заполнить это поле');
+          setTimeout(() => {
+            err.setAttribute('placeholder', 'Новая запись...');
+          }, 1000, err);
         }
       }
       if (e.target.classList.contains('btn-del')) {
         e.target.closest('.list-item').remove();
+        this.saveState();
+      }
+    });
+
+    // ------------------------ down ---------------------------
+    this.dom.addEventListener('mousedown', (e) => {
+      if (e.target.classList.contains('btn-del')) { return; }
+      const target = e.target.closest('.list-item');
+      if (!target) {
+        return;
+      }
+      e.preventDefault();
+      if (e.target.classList.contains('list-item')) {
+        e.target.style.cursor = 'grabbing';
+        this.source = target;
+        this.dragItem = this.source.cloneNode(true);
+        this.dragdX = e.pageX - e.target.offsetLeft;
+        this.dragY = e.pageY - e.target.offsetTop;
+        this.dragItem.style.top = `${e.target.offsetTop - 5}px`;
+        this.dragItem.style.left = `${e.target.offsetLeft + 5}px`;
+        this.dragItem.style.width = `${this.source.offsetWidth}px`;
+        this.dragItem.style.height = `${this.source.offsetHeight}px`;
+        this.dragItem.style.zIndex = '5000';
+        this.dragItem.style.position = 'absolute';
+        this.dragItem.style.opacity = '0.8';
+        this.dragItem.style.pointerEvents = 'none';
+        this.dom.append(this.dragItem);
+        this.source.classList.add('selected');
+      }
+    });
+
+    // ------------------------ up -----------------------------
+    this.dom.addEventListener('mouseup', (e) => {
+      if (!this.source) {
+        return;
+      }
+      e.preventDefault();
+      const insertItem = document.elementFromPoint(e.clientX, e.clientY);
+      if (insertItem.classList.contains('list-item-cap')) {
+        this.cap.replaceWith(this.source);
+        this.saveState();
+      }
+      if (this.cap) {
+        this.cap.remove();
+      }
+      this.dragItem.remove();
+      this.source.classList.remove('selected');
+      this.dragItem = null;
+      this.source = null;
+    });
+
+    // ------------------------ out -----------------------------
+    this.dom.addEventListener('mouseout', (e) => {
+      e.target.style.cursor = '';
+    });
+
+    // ----------------------- move ------------------------------
+    this.dom.addEventListener('mousemove', (e) => {
+      if (!this.source) {
+        return;
+      }
+      e.target.style.cursor = 'grabbing';
+      this.dragItem.style.top = `${e.pageY - this.dragY}px`;
+      this.dragItem.style.left = `${e.pageX - this.dragdX}px`;
+      const insertItem = document.elementFromPoint(e.clientX, e.clientY);
+      if (insertItem.classList.contains('list-item') && insertItem !== this.source) {
+        const midlle = insertItem.offsetHeight / 2;
+        const currentPos = e.pageY - insertItem.offsetTop;
+        this.cap.style.height = `${this.dragItem.offsetHeight}px`;
+        if (currentPos > midlle && insertItem.nextSibling !== this.source) {
+          insertItem.insertAdjacentElement('afterEnd', this.cap);
+        }
+        if (currentPos < midlle && insertItem.previousSibling !== this.source) {
+          insertItem.insertAdjacentElement('beforeBegin', this.cap);
+        }
+      }
+      if (insertItem.classList.contains('topic') && insertItem.closest('.list-block').querySelector('ul').childNodes.length === 0) {
+        insertItem.closest('.list-block').querySelector('ul').append(this.cap);
       }
     });
   }
@@ -177,5 +184,16 @@ export default class Trello {
       <input class="form-btn-submit" type="submit" value="Добавить"/>
       <div class="form-btn-cancel">✖</div>`;
     return form;
+  }
+
+  saveState() {
+    const arrSave = [];
+    const arrUl = [...this.dom.querySelectorAll('.list-block')];
+    for (let i = 0; i < arrUl.length; i += 1) {
+      const arrLi = [...arrUl[i].querySelectorAll('.list-item')];
+      const arrtext = arrLi.map((e) => e.innerText);
+      arrSave.push(arrtext);
+    }
+    localStorage.setItem('trello', JSON.stringify(arrSave));
   }
 }
